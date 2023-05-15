767febfe1747548a1213f0dc927bc4254b2dab79
s7markobates <marko.bates@s7codedesign.com>
2023-05-15 14:38:05 +0200

    Za sada sve u redu.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <h1>First bts app</h1>
       <p>Da vidim da je sve u redu sa git-om.</p>
+      <p>Radi sve, samo još mali detalji...</p>
     </div>
   );
 }
